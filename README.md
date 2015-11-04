4d104057d732d9d6af5f223f4f47cf1f7638fd6b
Yacine Haddad <yhaddad@cern.ch>
2015-11-04 11:22:21 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## produce a plot card
+This commands produce a plot card in json format:
+
+```bash
+./makeplotcard.py --load data/100000/output_VBFHToGG_M-125_13TeV_powheg_pythia8_numEvent100000_histos.root\
+	--out plotcard.json
+```
+this is for the use of [flashgg](https://github.com/cms-analysis/flashgg) type dumper trees
+
+
+
---
+++ makeplotcard.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+
+import ROOT
+import sys, os, glob, json
+
+version=0.1
+
+# --------------------
+# read arguments
+# --------------------
+def create_json(rootfile='file.root', treename='', jout=''):
+    """
+    [] Create a json template file for
+    plotng all the branches in a given
+    tree
+    """
+    dumper = treename.split("*")[0]
+    treerf = treename.split("*")[1]
+    print 'dumper', dumper
+    print 'treerf', treerf
+    root = ROOT.TFile.Open(rootfile)
+    tree_dir = root.Get(dumper+'trees')
+    
+    treenm = '' 
+    for a in tree_dir.GetListOfKeys():
+        if treerf in a.GetName():
+            treenm = a.GetName()
+    tree = ROOT.gDirectory.Get(dumper+'trees/'+treenm)
+    print dumper+'/trees/'+treenm
+    #if tree.GetEntries()==0:
+    #    print 'ERROR: the tree is empty ...'
+    # create list of variables
+    varlist={}
+    for var in tree.GetListOfLeaves():
+        varlist[var.GetTitle()]={'title':'','hist':'(100,0,100)','norm':False,'log':False}
+        # create a json file
+    samples = json.loads(open('samples.json').read())
+    samples_new = {}
+    color_count = 1;
+    for sam in samples:
+        if 'processes' in sam:
+            for proc in samples[sam]:
+                samples[sam][proc].append("%i"%color_count)
+                samples_new[proc] = samples[sam][proc] 
+                color_count = color_count + 1
+    
+    with open(jout, "w") as file:
+        json.dump({'variables':varlist, 'processes':samples_new}, file, indent=4)
+    file.close()
+        
+# --------------------
+# read arguments
+# --------------------
+from optparse import OptionParser
+
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option("-r", "--load", dest="rootfile",default='rootfile.root',
+                      help="create a plot card from tree", metavar="FILE")
+    parser.add_option("-t", "--tree", dest="treename",default='VBFMVADumper/*VBFDiJet',
+                      help="Dumper tree that you wnat to plot", metavar="FILE")
+    parser.add_option("-o", "--out", dest="jsonfile",default="plotcard.json",
+                      help="specify output json file")
+    
+    (options, args) = parser.parse_args()
+    create_json(options.rootfile,
+                options.treename,
+                options.jsonfile)
+
+    
+    
