7fc965ec9a1414eb2f67568c7931abbeb8032f59
Edward Scott <e.scott15@ic.ac.uk>
2016-07-17 16:20:01 +0200

    Slightly hacked heppi and plotcard, plotting scripts for weighted plots. Both QCD param and validation

---
+++ add_ICHEP2016_zplusjet.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#./plot -r plotcard/PujidSyst/eta0pt0.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-00
+#./plot -r plotcard/PujidSyst/eta1pt0.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-10
+#./plot -r plotcard/PujidSyst/eta2pt0.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-20
+#./plot -r plotcard/PujidSyst/eta3pt0.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-30
+#./plot -r plotcard/PujidSyst/eta0pt1.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-01
+#./plot -r plotcard/PujidSyst/eta1pt1.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-11
+#./plot -r plotcard/PujidSyst/eta2pt1.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-21
+#./plot -r plotcard/PujidSyst/eta3pt1.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-31
+#./plot -r plotcard/PujidSyst/eta0pt2.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-02
+#./plot -r plotcard/PujidSyst/eta1pt2.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-12
+#./plot -r plotcard/PujidSyst/eta2pt2.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-22
+#./plot -r plotcard/PujidSyst/eta3pt2.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-32
+
+#./plot -r plotcard/PujidSyst/eta0pt0.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-nozjcut-00
+#./plot -r plotcard/PujidSyst/eta1pt0.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-nozjcut-10
+#./plot -r plotcard/PujidSyst/eta2pt0.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-nozjcut-20
+#./plot -r plotcard/PujidSyst/eta3pt0.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-nozjcut-30
+#./plot -r plotcard/PujidSyst/eta0pt1.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-nozjcut-01
+#./plot -r plotcard/PujidSyst/eta1pt1.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-nozjcut-11
+#./plot -r plotcard/PujidSyst/eta2pt1.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-nozjcut-21
+#./plot -r plotcard/PujidSyst/eta3pt1.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-nozjcut-31
+#./plot -r plotcard/PujidSyst/eta0pt2.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-nozjcut-02
+#./plot -r plotcard/PujidSyst/eta1pt2.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-nozjcut-12
+#./plot -r plotcard/PujidSyst/eta2pt2.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-nozjcut-22
+#./plot -r plotcard/PujidSyst/eta3pt2.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_pujid --label pujid-syst-nozjcut-32
+
+./plot -r plotcard/PujidSyst/eta0pt0.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_rms --label rms-syst-nozjcut-00
+./plot -r plotcard/PujidSyst/eta1pt0.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_rms --label rms-syst-nozjcut-10
+./plot -r plotcard/PujidSyst/eta2pt0.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_rms --label rms-syst-nozjcut-20
+./plot -r plotcard/PujidSyst/eta3pt0.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_rms --label rms-syst-nozjcut-30
+./plot -r plotcard/PujidSyst/eta0pt1.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_rms --label rms-syst-nozjcut-01
+./plot -r plotcard/PujidSyst/eta1pt1.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_rms --label rms-syst-nozjcut-11
+./plot -r plotcard/PujidSyst/eta2pt1.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_rms --label rms-syst-nozjcut-21
+./plot -r plotcard/PujidSyst/eta3pt1.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_rms --label rms-syst-nozjcut-31
+./plot -r plotcard/PujidSyst/eta0pt2.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_rms --label rms-syst-nozjcut-02
+./plot -r plotcard/PujidSyst/eta1pt2.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_rms --label rms-syst-nozjcut-12
+./plot -r plotcard/PujidSyst/eta2pt2.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_rms --label rms-syst-nozjcut-22
+./plot -r plotcard/PujidSyst/eta3pt2.json -s /afs/cern.ch/work/e/escott/public/ValidationICHEP/ZPlusJet/ -v jet_rms --label rms-syst-nozjcut-32
+
+cp plots/* ~/www/ICHEP16/ZPlusJet/
+rm plots/*
